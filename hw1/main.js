8c22105f8c96dfd1419caab09b2f22fdb23be20f
volodya92dutchuk <dutchuk.volodya@gmail.com>
2023-01-28 12:01:38 +0200

    hw

---
+++ hw1/main.js
@@ -0,0 +1,53 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+// let str = 'hello';
+// console.log(str);
+//
+// str = 'owu';
+// console.log(str);
+//
+// str = 'com';
+// console.log(str);
+//
+// str = 'ua';
+// console.log(str);
+//
+// let num = 1;
+// console.log(num);
+//
+// num = 123;
+// console.log(123);
+//
+// const PI = 3.14;
+// console.log(PI);
+//
+// let bool1 = true;
+// console.log(bool1);
+//
+// let bool2 = false;
+// console.log(bool1);
+
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+// let lastName = 'Dutchuk';
+// let firstName = 'Volodymyr';
+// let middleName = 'Vasylovych';
+// let result = lastName + ' ' +firstName + ' ' +middleName;
+// console.log(result);
+// let result =` ${lastName} ${firstName} ${middleName}`;
+// console.log(result);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+// let a = 100;
+// console.log(typeof a);
+//
+// let b = '100';
+// console.log(typeof b);
+//
+// let c = true;
+// console.log(typeof c);
+
+// let str =
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
