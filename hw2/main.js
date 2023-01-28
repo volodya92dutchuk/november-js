a2d3db1981823b32e6579b920ca432e25eb264e6
volodya92dutchuk <dutchuk.volodya@gmail.com>
2023-01-28 17:18:41 +0200

    hw2

---
+++ hw2/main.js
@@ -0,0 +1,56 @@
+// Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// let arr = [true, false, 123, 3.14, 'okten', 'hello', -730, 'world', 'vova', 850];
+// arr[3]=3.15;0
+// arr[10]='chao';
+// console.log(arr.length);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// let books = {
+//     tittle:'Harry Potter',
+//     pageCount:777,
+//     genre:'fantasy'
+// }
+// console.log(books)
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+// let books = {
+//     tittle: 'Harry Potter',
+//     pageCount: 777,
+//     genre: 'fantasy',
+//     authors: [
+//         {
+//             name: 'Frank',
+//             age: 55
+//         },
+//         {
+//             name: 'Jason',
+//             age: 85
+//         }
+//     ]
+// }
+// console.log(books)
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+// let users = [
+//     {
+//         name: 'Anton',
+//         age: 45,
+//         password: 12
+//     },
+//     {
+//         name: 'Petya',
+//         age: 54,
+//         password: 23
+//
+//     },
+//     {
+//         name: 'Taras',
+//         age: 77,
+//         password: 54
+//
+//     }
+// ]
+// // console.log(users[0].password)
+// // console.log(users[1].password)
+// console.log(users[2])
